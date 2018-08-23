2290091f3458fc80ee164797ec467006230b5396
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-23 14:34:49 -0700

    add a nav btw new form and index page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions'
+import { Link } from 'react-router-dom'
 
 class PostsIndex extends Component {
 	componentDidMount() {
@@ -21,6 +22,11 @@ class PostsIndex extends Component {
 	render() {
 		return (
 			<div>Posts Index
+				<div className = "text-xs-right">
+	    			<Link className = "btn btn-primary" to = "/posts/new">
+	    				Add a post
+	    			</Link>
+    			</div>
 				<h3>Posts</h3>
 				<ul className = "list-group" >
 					{this.renderPosts()}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Link } from 'react-router-dom'
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import promise from 'redux-promise';
