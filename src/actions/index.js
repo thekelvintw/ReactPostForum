0ceab0231fec7733828cadc150f431382e3fd71d
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-23 13:32:04 -0700

    put post lists on the page

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const Root_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=PAPERCLICK1234'   
+const API_KEY = '?key=PAPERCLICK123'   
 
 export function fetchPosts() {
 	const request = axios.get(`${Root_URL}/posts${API_KEY}`);
