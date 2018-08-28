8838592d391449fe1c12a43e7be52da1bd89b8e3
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-28 15:22:35 -0700

    Impletement the delete , complete the app

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 
 export default function (state = {} , action) {
 	switch (action.type) {
+	case DELETE_POST:
+		// lodash method
+		return  _.omit(state, action.payload)
 	case FETCH_POST:
 		// const post = action.payload.data;
 		// // taking exisit state
